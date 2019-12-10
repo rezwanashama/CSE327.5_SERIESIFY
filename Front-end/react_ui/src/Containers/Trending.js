2d5c99187eff810409c39f2373299058e603985d
MdMuannaZilan <mdzilan30@gmail.com>
2019-12-10 21:35:52 +0600

    Trending page

--- Front-end/react_ui/src/Containers/Trending.js
+++ Front-end/react_ui/src/Containers/Trending.js
@@ -5,7 +5,8 @@ export default class Trending extends Component {
     render() {
         return (
             <div>
-                
+                <SeriesListView header="Trending" apicall={'https://api.themoviedb.org/3/trending/tv/week?api_key=a5c1a2920eaf594c07fef4fe581f2166'}/>
+
             </div>
         )
     }
