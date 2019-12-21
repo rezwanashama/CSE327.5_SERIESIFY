09b6f0d299d5b7a9dd5e99869b78ec1623533e32
MdMuannaZilan <mdzilan30@gmail.com>
2019-12-22 01:15:20 +0600

    intial documentation

--- source/conf.py
+++ source/conf.py
@@ -27,9 +27,14 @@ author = 'Zilan'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
+extensions = ['recommonmark'
 ]
-
+###
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
